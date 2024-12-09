f3182113c91a625271a1f015062db71925224c7e
bong <jungcome7@gmail.com>
2024-12-09 10:46:05 +0900

    env: shadcn Button 컴포넌트 설치 및 관련 설정 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
-  return <div className="text-6xl text-red-300">ㅁㅇ</div>;
+  return (
+    <>
+      <div className="text-6xl text-red-300">ㅁㅇ</div>
+      <Button onClick={() => alert('hi')}>Button</Button>
+    </>
+  );
 }
