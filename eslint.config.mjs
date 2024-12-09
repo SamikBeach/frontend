7a847e0d9e0bd19f450067c35cfd5d73942a9e8e
bong <jungcome7@gmail.com>
2024-12-09 10:45:42 +0900

    env: eslint 룰 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,18 @@ export default [
       semi: ['error', 'always'],
       'no-trailing-spaces': ['error', { skipBlankLines: false }],
       'no-multiple-empty-lines': ['error', { max: 1 }],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
 ];
