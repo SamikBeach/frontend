7e9c99840ad2e29234693ba65ff896e3d923c186
bong <jungcome7@gmail.com>
2024-12-10 12:38:45 +0900

    feat: Header 검색 바 및 다이얼로그 UI 레이아웃 잡기

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,13 @@
 import '@/styles/global.css';
-import { Input } from '../ui/input';
 import { LeftSlot } from './LeftSlot';
 import { RightSlot } from './RightSlot';
+import SearchBar from './SearchBar/SearchBar';
 
 export default function Header() {
   return (
     <header className="flex h-[56px] items-center justify-between border-b-[1px] px-4">
       <LeftSlot />
-      <Input
-        className="max-w-96 rounded-full"
-        placeholder="책이나 작가를 검색해 보세요..."
-      />
+      <SearchBar />
       <RightSlot />
     </header>
   );
---
+++ src/components/Header/SearchBar/SearchBar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import { useKeyPressEvent } from 'react-use';
+
+export default function SearchBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useKeyPressEvent('/', () => setIsOpen(true));
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger>
+        <Input
+          className="absolute left-1/2 top-[27px] w-60 -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full placeholder:text-gray-400"
+          placeholder="/를 눌러 검색해보세요."
+        />
+      </DialogTrigger>
+      <DialogContent
+        className="top-[6px] w-[600px] translate-y-0 bg-white sm:max-w-[600px]"
+        overlayClassName="bg-black/10"
+        closeClassName="hidden"
+      >
+        <Input
+          className="border-1 w-full bg-gray-100 focus-visible:ring-0"
+          placeholder="책이나 작가를 검색하세요."
+        />
+        <DialogTitle>검색 결과 블라블라블라</DialogTitle>
+        {/* <DialogDescription>디스크립션</DialogDescription> */}
+      </DialogContent>
+    </Dialog>
+  );
+}
