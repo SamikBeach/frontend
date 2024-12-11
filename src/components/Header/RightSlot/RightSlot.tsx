f9548869daccd4f6f83376f142a1d0c8d963420c
bong <jungcome7@gmail.com>
2024-12-11 10:59:45 +0900

    feat: shadcn Dropdown Menu 컴포넌트 추가 & 아바타 드롭다운 UI 추가

--- src/components/Header/RightSlot/RightSlot.tsx
+++ src/components/Header/RightSlot/RightSlot.tsx
@@ -1,14 +1,38 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { LogOut, Settings, User } from 'lucide-react';
 
 export default function RightSlot() {
   return (
-    <Avatar className="h-8 w-8 cursor-pointer">
-      <AvatarImage
-        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt="User avatar"
-        className="rounded-full"
-      />
-      <AvatarFallback>UN</AvatarFallback>
-    </Avatar>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar className="relative h-8 w-8 cursor-pointer after:absolute after:inset-0 after:rounded-full after:border-2 after:border-primary/50 after:opacity-0 after:transition-opacity hover:after:opacity-100">
+          <AvatarImage
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt="User avatar"
+            className="rounded-full"
+          />
+          <AvatarFallback>UN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem>
+          <User className="mr-2 h-4 w-4" />내 프로필
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Settings className="mr-2 h-4 w-4" />
+          설정
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-red-600 hover:!text-red-600">
+          <LogOut className="mr-2 h-4 w-4" />
+          로그아웃
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
