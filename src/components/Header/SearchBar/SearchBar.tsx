2ffee7e0addc75a36bde9c6ca21449c6f2fad47e
bong <jungcome7@gmail.com>
2024-12-12 11:15:01 +0900

    feat: SearchBar 스타일 수정

--- src/components/Header/SearchBar/SearchBar.tsx
+++ src/components/Header/SearchBar/SearchBar.tsx
@@ -26,12 +26,12 @@ export default function SearchBar() {
         />
       </DialogTrigger>
       <DialogContent
-        className="top-[6px] w-[600px] translate-y-0 bg-white sm:max-w-[600px]"
+        className="top-[6px] w-[600px] translate-y-0 bg-white px-3 pt-3 sm:max-w-[600px]"
         overlayClassName="bg-black/10"
         closeClassName="hidden"
       >
         <Input
-          className="border-1 w-full bg-gray-100 focus-visible:ring-0"
+          className="border-1 -mt-1 w-full bg-gray-100 focus-visible:ring-0"
           placeholder="책이나 작가를 검색하세요."
         />
         <DialogTitle>검색 결과 블라블라블라</DialogTitle>
