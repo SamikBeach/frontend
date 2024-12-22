a84af7d9f9d0a5380b16b709fd9f5c8dc03cdc91
bong <jungcome7@gmail.com>
2024-12-22 18:11:19 +0900

    feat: next intl request config 추가 & 루트에 NextIntlClientProvider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,27 @@
 import Header from '@/components/Header/Header';
 import { LeftSidebar } from '@/components/LeftSidebar';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const messages = await getMessages();
+
   return (
     <html lang="ko">
       <body>
-        <div className="flex h-screen flex-col">
-          <Header />
-          <div className="flex flex-1">
-            <LeftSidebar />
-            <main className="flex-1">{children}</main>
+        <NextIntlClientProvider messages={messages}>
+          <div className="flex h-screen flex-col">
+            <Header />
+            <div className="flex flex-1">
+              <LeftSidebar />
+              <main className="flex-1">{children}</main>
+            </div>
           </div>
-        </div>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
