6d5a56b5408a6ab174b332b2e2a07361c2aec731
bong <jungcome7@gmail.com>
2025-01-05 17:51:03 +0900

    feat: LoginDialog 내 LoginForm 리디자인

--- src/components/LoginDialog/LoginDialog.tsx
+++ src/components/LoginDialog/LoginDialog.tsx
@@ -1,5 +1,6 @@
-import { useTranslations } from 'next-intl';
+import Google from '@/svgs/google';
 import { ComponentPropsWithoutRef } from 'react';
+import { Logo } from '../Logo';
 import { Button } from '../ui/button';
 import {
   Dialog,
@@ -15,44 +16,41 @@ interface Props extends ComponentPropsWithoutRef<typeof Dialog> {
 }
 
 export default function LoginDialog(props: Props) {
-  const t = useTranslations('Common');
-
   return (
     <Dialog {...props}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader className="flex items-center">
-          <img src="/logo.png" alt="삼익" className="mb-8 h-8" />
+      <DialogContent className="w-96" overlayClassName="bg-black/10">
+        <DialogHeader className="flex items-center py-6">
+          <Logo />
         </DialogHeader>
-        <div className="flex flex-col gap-4">
-          <Input
-            placeholder="이메일을 입력해 주세요."
-            type="email"
-            className="w-full rounded-md border border-gray-200 p-4"
-          />
-          <Input
-            placeholder="비밀번호를 입력해 주세요."
-            type="password"
-            className="w-full rounded-md border border-gray-200 p-4"
-          />
-          <Button
-            type="submit"
-            className="w-full rounded-md bg-[#18181b] p-4 text-white hover:bg-[#27272a]"
-          >
-            로그인
-          </Button>
-          <Button
-            variant="outline"
-            className="flex w-full items-center justify-center gap-2 rounded-md border border-gray-200 p-4"
-          >
-            <img src="/google.png" alt="Google" className="h-5 w-5" />G 구글
-            계정으로 로그인
-          </Button>
-          <div className="mt-2 flex flex-col justify-center gap-4 text-sm text-gray-600">
-            <button className="hover:underline">비밀번호를 잊으셨나요?</button>
+        <div className="flex flex-col gap-7">
+          <div className="flex flex-col gap-3">
+            <Input
+              placeholder="이메일을 입력해 주세요."
+              type="email"
+              className="w-full rounded-md border border-gray-200 p-4"
+            />
+            <Input
+              placeholder="비밀번호를 입력해 주세요."
+              type="password"
+              className="w-full rounded-md border border-gray-200 p-4"
+            />
+            <Button type="submit">로그인</Button>
+            <Button variant="outline">
+              <Google />
+              구글 계정으로 로그인
+            </Button>
+          </div>
+          <div className="mb-5 flex flex-col items-center gap-1 text-sm">
+            <Button variant="link" className="h-6 py-0 text-gray-500">
+              비밀번호를 잊으셨나요?
+            </Button>
 
-            <button className="hover:underline">
-              아직 계정이 없으신가요? 회원가입
-            </button>
+            <div className="flex items-center">
+              아직 계정이 없으신가요?
+              <Button variant="link" className="h-6 p-1 text-gray-500">
+                회원가입
+              </Button>
+            </div>
           </div>
         </div>
       </DialogContent>
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,7 +4,7 @@ interface LogoProps {
 
 export default function Logo({ className }: LogoProps) {
   return (
-    <div className={`inline-block cursor-pointer ${className || ''}`}>
+    <div className={`inline-block ${className || ''}`}>
       <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-base font-extrabold text-transparent">
         삼익비치
       </h1>
