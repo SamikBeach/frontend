1eb35dc43a7c8c4e05fe49e5be87486c801b715a
bong <jungcome7@gmail.com>
2025-01-05 20:45:06 +0900

    fix: fix warning & errors

--- src/components/ReviewDialog/ReviewDialog.tsx
+++ src/components/ReviewDialog/ReviewDialog.tsx
@@ -1,8 +1,4 @@
-import {
-  DialogDescription,
-  DialogProps,
-  DialogTitle,
-} from '@radix-ui/react-dialog';
+import { DialogProps, DialogTitle } from '@radix-ui/react-dialog';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog';
 
@@ -15,11 +11,26 @@ export default function ReviewDialog({ children, ...props }: Props) {
       <DialogContent
         overlayClassName="bg-black/10"
         className="h-[94vh] w-[900px] min-w-[900px] overflow-y-auto"
+        aria-describedby={undefined}
       >
         <DialogTitle className="text-2xl font-bold">
           차라투스트라 읽지마세요
         </DialogTitle>
-        <DialogDescription>
+        <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2">
+            <img
+              src="https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9788970132099.jpg"
+              className="h-[40px] rounded-sm"
+            />
+            <div className="flex flex-col">
+              <p className="text-sm font-semibold">
+                차라투스트라는 이렇게 말했다
+              </p>
+              <p className="text-xs text-gray-500">
+                프리드리히 니체 · 민음사 · 2021
+              </p>
+            </div>
+          </div>
           <div className="flex items-center gap-2">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
@@ -27,8 +38,8 @@ export default function ReviewDialog({ children, ...props }: Props) {
             </Avatar>
             <p className="font-medium">BonggeunJeong</p>
           </div>
-          <p>2021년 3월 12일 수요일 12시 33분 11초</p>
-        </DialogDescription>
+          <p className="text-gray-500">2021년 3월 12일 수요일 12시 33분 11초</p>
+        </div>
         <div>1</div>
         <div>1</div>
         <div>1</div>
