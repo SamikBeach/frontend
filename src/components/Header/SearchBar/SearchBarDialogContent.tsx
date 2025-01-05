b4e5ebf17e6f1b21f101174aa25a6b55e0a8ea8b
bong <jungcome7@gmail.com>
2025-01-05 14:38:13 +0900

    feat: 최근 검색 목록 표시

--- src/components/Header/SearchBar/SearchBarDialogContent.tsx
+++ src/components/Header/SearchBar/SearchBarDialogContent.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import AuthorItem from './AuthorItem';
 import BookItem from './BookItem';
 
 export default function SearchBarDialogContent() {
+  // TODO: jotai atom으로 변경
+  const [searchValue, _] = useState('');
+
+  if (searchValue === '') {
+    return (
+      <div className="flex flex-col gap-2">
+        <div className="px-2 text-sm font-semibold">최근 검색</div>
+        <div className="flex flex-col">
+          <BookItem />
+          <AuthorItem />
+          <BookItem />
+          <BookItem />
+          <AuthorItem />
+          <AuthorItem />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="flex flex-col gap-3">
