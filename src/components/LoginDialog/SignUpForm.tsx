d17ceaf779bfd7f488594373166b04647a44eea7
bong <jungcome7@gmail.com>
2025-01-08 20:17:52 +0900

    fix: 불필요한 코드 제거

--- src/components/LoginDialog/SignUpForm.tsx
+++ src/components/LoginDialog/SignUpForm.tsx
@@ -26,7 +26,6 @@ export default function SignUpForm({ onClickGoToLogin, onSuccess }: Props) {
   const {
     control,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<SignUpFormData>({
     defaultValues: {
@@ -45,28 +44,6 @@ export default function SignUpForm({ onClickGoToLogin, onSuccess }: Props) {
         value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
         message: '올바른 이메일 형식이 아닙니다',
       },
-    },
-  });
-
-  const { field: passwordField } = useController({
-    name: 'password',
-    control,
-    rules: {
-      required: '비밀번호를 입력해주세요',
-      minLength: {
-        value: 6,
-        message: '비밀번호는 최소 6자 이상이어야 합니다',
-      },
-    },
-  });
-
-  const { field: confirmPasswordField } = useController({
-    name: 'confirmPassword',
-    control,
-    rules: {
-      required: '비밀번호 확인을 입력해주세요',
-      validate: value =>
-        value === watch('password') || '비밀번호가 일치하지 않습니다',
     },
   });
 
@@ -121,32 +98,6 @@ export default function SignUpForm({ onClickGoToLogin, onSuccess }: Props) {
           />
           {errors.email && (
             <span className="text-xs text-red-500">{errors.email.message}</span>
-          )}
-        </div>
-        <div className="flex flex-col gap-1">
-          <Input
-            {...passwordField}
-            placeholder="비밀번호를 입력해 주세요."
-            type="password"
-            className="w-full rounded-md border border-gray-200 p-4"
-          />
-          {errors.password && (
-            <span className="text-xs text-red-500">
-              {errors.password.message}
-            </span>
-          )}
-        </div>
-        <div className="flex flex-col gap-1">
-          <Input
-            {...confirmPasswordField}
-            placeholder="비밀번호를 다시 입력해 주세요."
-            type="password"
-            className="w-full rounded-md border border-gray-200 p-4"
-          />
-          {errors.confirmPassword && (
-            <span className="text-xs text-red-500">
-              {errors.confirmPassword.message}
-            </span>
           )}
         </div>
         <Button type="submit">회원가입</Button>
