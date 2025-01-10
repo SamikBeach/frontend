2cc5f93a58a7e685c6eec4536b114b38cb925051
bong <jungcome7@gmail.com>
2025-01-10 22:42:09 +0900

    refactor: URL query param 초기화 로직 분리
    - URL query param 기반 atom 초기화 로직을 useInitializeAtoms 훅으로 분리
    - 초기화 로직을 page 컴포넌트로 이동
    - FilterInitializer 컴포넌트 제거

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import { useInitializeAtoms } from '@/hooks/useInitializeAtoms';
 import BookList from './BookList';
 import Tabs from './Tabs/Tabs';
 
-export default function Books() {
+export default function BooksPage() {
+  useInitializeAtoms();
+
   return (
     <>
       <Tabs />
