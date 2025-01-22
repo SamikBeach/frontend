1b4021282d7ec531ca99e9e4b1e5edf92591c276
bong <jungcome7@gmail.com>
2025-01-22 20:14:59 +0900

    fix: 코멘트 에디터 하단 마진 추가

--- src/app/review/[id]/page.tsx
+++ src/app/review/[id]/page.tsx
@@ -54,7 +54,7 @@ export default function ReviewPage() {
         onReply={user => setReplyToUser(user)}
         scrollableTarget="dialog-content"
       />
-      <div className="sticky bottom-0 bg-white pt-4">
+      <div className="sticky bottom-10 bg-white pt-4">
         <div className="relative">
           <div className="absolute -bottom-10 -left-10 -right-10 -top-4 bg-white shadow-[0_-8px_12px_0px_white]" />
           <div className="relative">
