46312fc6e8b3ee18a2c3321c3ea60ff2275b506e
bong <jungcome7@gmail.com>
2025-01-22 20:13:32 +0900

    fix: 스켈레톤 스타일  수정

--- src/app/review/[id]/ReviewInfo.tsx
+++ src/app/review/[id]/ReviewInfo.tsx
@@ -185,24 +185,25 @@ function ReviewInfoContent({ reviewId, commentListRef }: Props) {
 function ReviewInfoSkeleton() {
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex items-start justify-between gap-8">
-        <div className="flex flex-col gap-2">
-          <Skeleton className="h-9 w-96" />
+      <div className="flex items-start justify-between gap-2">
+        <div className="flex flex-col gap-4">
+          <div className="flex items-start gap-4">
+            <Skeleton className="h-9 w-96" />
+            <div className="flex shrink-0 items-center gap-2 rounded-lg bg-gray-50 px-2 py-1">
+              <Skeleton className="h-7 w-5 rounded-sm" />
+              <div className="flex flex-col gap-0.5">
+                <Skeleton className="h-3 w-24" />
+                <Skeleton className="h-2.5 w-20" />
+              </div>
+            </div>
+          </div>
           <div className="flex items-center gap-1">
             <div className="flex items-center gap-2">
-              <Skeleton className="h-7 w-7 rounded-full" />
-              <Skeleton className="h-5 w-20" />
+              <Skeleton className="h-6 w-6 rounded-full" />
+              <Skeleton className="h-4 w-20" />
             </div>
-            <Skeleton className="h-5 w-2" />
-            <Skeleton className="h-5 w-36" />
-          </div>
-        </div>
-
-        <div className="flex shrink-0 items-center gap-2 rounded-lg bg-gray-50 p-2.5">
-          <Skeleton className="h-14 w-10 rounded-sm" />
-          <div className="flex flex-col gap-0.5">
-            <Skeleton className="h-4 w-24" />
-            <Skeleton className="h-3 w-20" />
+            <Skeleton className="h-4 w-2" />
+            <Skeleton className="h-4 w-36" />
           </div>
         </div>
       </div>
@@ -214,8 +215,8 @@ function ReviewInfoSkeleton() {
       </div>
 
       <div className="flex justify-center gap-2">
-        <Skeleton className="h-9 w-20" />
-        <Skeleton className="h-9 w-20" />
+        <Skeleton className="h-9 w-20 rounded-full" />
+        <Skeleton className="h-9 w-20 rounded-full" />
       </div>
     </div>
   );
