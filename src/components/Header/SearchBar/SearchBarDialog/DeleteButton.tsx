2a1e988c63f54f306019c265d478ade07d2dfc60
bong <jungcome7@gmail.com>
2025-01-26 18:44:23 +0900

    fix: SearchBar 스타일 수정

--- src/components/Header/SearchBar/SearchBarDialog/DeleteButton.tsx
+++ src/components/Header/SearchBar/SearchBarDialog/DeleteButton.tsx
@@ -15,7 +15,7 @@ export default function DeleteButton({ onClick }: Props) {
 
         onClick(e);
       }}
-      className="absolute right-2 top-1/2 h-8 w-8 -translate-y-1/2 hover:bg-gray-200 max-sm:opacity-100 sm:opacity-0 sm:group-hover:opacity-100"
+      className="absolute right-2 top-1/2 h-8 w-8 -translate-y-1/2 hover:bg-gray-200 max-md:opacity-100 md:opacity-0 md:group-hover:opacity-100"
     >
       <X className="h-4 w-4 text-muted-foreground" />
     </Button>
