cab0d2d974a3eb1974b6315e220590d2f4c4b665
bong <jungcome7@gmail.com>
2025-01-29 01:17:42 +0900

    feat: .heic 업로드 허용

--- src/app/user/[id]/UserInfo.tsx
+++ src/app/user/[id]/UserInfo.tsx
@@ -127,7 +127,7 @@ function UserInfoContent({ userId }: Props) {
                   </span>
                   <Input
                     type="file"
-                    accept="image/*"
+                    accept="image/*,.heic"
                     className="hidden"
                     onChange={handleImageUpload}
                   />
@@ -152,7 +152,7 @@ function UserInfoContent({ userId }: Props) {
                       onClick={() => {
                         const input = document.createElement('input');
                         input.type = 'file';
-                        input.accept = 'image/*';
+                        input.accept = 'image/*,.heic';
                         input.onchange = e => {
                           const file = (e.target as HTMLInputElement)
                             .files?.[0];
