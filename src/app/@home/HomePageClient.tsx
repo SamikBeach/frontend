2781c7d5f4870e5cd87036a43d1c44142f75542e
bong <jungcome7@gmail.com>
2025-01-28 23:33:39 +0900

    refactor: 홈페이지 구조 개선 및 메타데이터 생성 방식 통일
    - @home 디렉토리의 구조를 다른 페이지들과 동일하게 변경
    - HomePageClient 컴포넌트 분리
    - generateMetadata 함수를 통한 동적 메타데이터 생성 구현
    - 페이지 간 일관된 메타데이터 동작 보장

---
+++ src/app/@home/HomePageClient.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import FeedList from './FeedList';
+
+export default function HomePageClient() {
+  return (
+    <div className="mx-auto max-w-5xl">
+      <FeedList />
+    </div>
+  );
+} 
--- src/app/@home/page.tsx
+++ src/app/@home/page.tsx
@@ -1,9 +1,25 @@
-import FeedList from './FeedList';
+import { Metadata } from 'next';
+import HomePageClient from './HomePageClient';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: '고전산책',
+    description: '시대를 초월한 지혜, 고전과 함께하는 새로운 독서 경험',
+    openGraph: {
+      title: '고전산책',
+      description: '시대를 초월한 지혜, 고전과 함께하는 새로운 독서 경험',
+      url: 'https://classicswalk.com',
+      images: ['/images/og-image.png'],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: '고전산책',
+      description: '시대를 초월한 지혜, 고전과 함께하는 새로운 독서 경험',
+      images: ['/images/og-image.png'],
+    },
+  };
+}
 
 export default function HomePage() {
-  return (
-    <div className="mx-auto max-w-5xl">
-      <FeedList />
-    </div>
-  );
+  return <HomePageClient />;
 }
