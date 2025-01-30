deb4cdd6a6a99fd5dc98d031116290bdd4fb400f
bong <jungcome7@gmail.com>
2025-01-30 17:57:45 +0900

    feat: 리뷰 페이지에 댓글 작성 기능 추가
    - 기존 레이아웃 유지
    - 댓글 작성 및 답글 기능 추가
    - 댓글 작성 성공/실패 알림 추가
    - 페이지 컴포넌트 타입 수정

--- src/app/review/[id]/ReviewPageClient.tsx
+++ src/app/review/[id]/ReviewPageClient.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { reviewApi } from '@/apis/review/review';
+import CommentEditor from '@/components/CommentEditor/CommentEditor';
+import { toast } from '@/components/ui/sonner';
+import { useCommentQueryData } from '@/hooks/queries/useCommentQueryData';
+import { useMutation } from '@tanstack/react-query';
 import { useRef, useState } from 'react';
 import CommentList from './CommentList';
 import ReviewInfo from './ReviewInfo';
@@ -14,9 +19,31 @@ export default function ReviewPageClient({ reviewId }: Props) {
     null
   );
 
+  const { createCommentQueryData } = useCommentQueryData();
+
+  const { mutate: createComment } = useMutation({
+    mutationFn: (comment: string) => {
+      return reviewApi.createComment(reviewId, { content: comment });
+    },
+    onSuccess: response => {
+      createCommentQueryData({ reviewId, comment: response.data });
+
+      // 새로운 댓글은 항상 목록 가장 앞에 추가되므로 첫 번째 댓글로 스크롤
+      setTimeout(() => {
+        commentListRef.current?.scrollIntoView({ behavior: 'smooth' });
+      }, 100);
+
+      toast.success('댓글이 작성되었습니다.');
+      setReplyToUser(null);
+    },
+    onError: () => {
+      toast.error('댓글 작성에 실패했습니다.');
+    },
+  });
+
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex min-h-[calc(100vh-234px)] flex-col">
+      <div className="flex min-h-[calc(100vh-120px)] flex-col gap-10">
         <ReviewInfo reviewId={reviewId} commentListRef={commentListRef} />
         <CommentList
           ref={commentListRef}
@@ -24,6 +51,17 @@ export default function ReviewPageClient({ reviewId }: Props) {
           onReply={user => setReplyToUser(user)}
           scrollableTarget="dialog-content"
         />
+        <div className="sticky bottom-10 bg-white pt-4">
+          <div className="relative">
+            <div className="absolute -bottom-10 -left-10 -right-10 -top-4 bg-white shadow-[0_-8px_12px_0px_white]" />
+            <div className="relative">
+              <CommentEditor
+                onSubmit={createComment}
+                replyToUser={replyToUser ?? undefined}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
