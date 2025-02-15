d9edab367637b5cbbd31f7b478ff251efd184628
bong <jungcome7@gmail.com>
2025-02-15 14:05:10 +0900

    fix: 애플 로고 SVG 크기 조정
    - SVG height를 18에서 22로 수정
    - viewBox를 0 0 18 22로 변경하여 로고 전체가 보이도록 수정

--- src/svgs/apple.tsx
+++ src/svgs/apple.tsx
@@ -2,8 +2,8 @@ export default function Apple() {
   return (
     <svg
       width="18"
-      height="18"
-      viewBox="0 0 18 18"
+      height="22"
+      viewBox="0 0 18 22"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className="mr-2"
